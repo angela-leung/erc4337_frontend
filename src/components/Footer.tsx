b2cd52832a1065578bf76cc241a259a943d29207
Angela Leung <angelaleung49@gmail.com>
2024-03-09 18:40:07 +0800

    feat: scaffold pages; setup login page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+  return (
+    <div className="text-center" style={{ marginBottom: '2rem' }}>
+      <p style={{ color: '#999' }}>
+        A ERC-4337 experiment with subscription built by @Angela
+      </p>
+      <p style={{ color: '#999', marginTop: '5px' }}>
+        <a href='#' target='_blank'>
+          Github Repo
+        </a>{' '}
+        · <a href='#'>Contract sources</a>
+      </p>
+    </div>
+  )
+}
